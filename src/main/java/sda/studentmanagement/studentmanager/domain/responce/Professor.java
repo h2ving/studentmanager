8638c5aa5b06170d4977fcdcf0090b1ac751c789
TAAVI <Taavijappinen@mail.com>
2022-03-27 13:29:55 +0300

    ??

--- src/main/java/sda/studentmanagement/studentmanager/domain/responce/Professor.java
+++ src/main/java/sda/studentmanagement/studentmanager/domain/responce/Professor.java
@@ -1,13 +1,17 @@
 package sda.studentmanagement.studentmanager.domain.responce;
 
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
 import java.util.Date;
 
 @Entity
+@Data
 public class Professor {
 
     @Id
@@ -37,70 +41,6 @@ public class Professor {
 
 
     public Professor() {
-
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getJoinedDate() {
-        return joinedDate;
-    }
-
-    public void setJoinedDate(Date joinedDate) {
-        this.joinedDate = joinedDate;
-    }
 }
--- src/main/java/sda/studentmanagement/studentmanager/domain/responce/Student.java
+++ src/main/java/sda/studentmanagement/studentmanager/domain/responce/Student.java
@@ -1,85 +1,42 @@
 package sda.studentmanagement.studentmanager.domain.responce;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
 import java.util.Date;
 
 @Entity
+@Data
 public class Student {
 
-
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column
     private String firstName;
+
+    @Column
     private String lastName;
+
+    @Column
     private Date dob;
+
+    @Column
     private String mobileNo;
+
+    @Column
     private String email;
+
+    @Column
     private String password;
+
+    @Column
     private Date joinedData;
 
     public Student() {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getJoinedData() {
-        return joinedData;
-    }
-
-    public void setJoinedData(Date joinedData) {
-        this.joinedData = joinedData;
-    }
 }
